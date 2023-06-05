19ccc28fdf7062b079c97ebaf14def829a6402df
samircastro27 <93741472+samircastro27@users.noreply.github.com>
2023-06-04 23:41:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 require('dotenv').config();
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.get("/saludos", function(req, res){
     res.send("Hola desde express");
